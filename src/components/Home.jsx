062485a4c16d8345f0492336a59a97d67d7de86b
Diego Carvalho <95982064+Diegocchaves@users.noreply.github.com>
2023-12-26 21:28:58 +0100

    change on home descriptions(original description)

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,15 +7,13 @@ const Home = () => {
     <div name='home' className='w-full h-screen bg-[#0a192f]' >
       {/* Container */}
       <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
-        <p className='text-[#28bdec]'>Hola, me llamo</p>
+        <p className='text-[#28bdec]'>Hi, my name is</p>
         <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6fd]'>Diego Carvalho</h1>
-        <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>Soy un asistente de almacén.</h2>
-        <p className='text-[#8892b0] py-4 max-w-[700px]'>Con una sólida experiencia como asistente de almacén, he desempeñado un papel fundamental en la eficiente gestión de operaciones logísticas. Mi enfoque se centra en la recepción, clasificación y distribución efectiva de productos, asegurando un flujo de trabajo suave y organizado.
-          Soy hábil en la manipulación segura de mercancías y en la utilización de sistemas de inventario para mantener registros precisos. Trabajar en equipo es fundamental para mí, y disfruto colaborando con colegas para garantizar un entorno de almacén productivo y ordenado.
-          Mi compromiso es contribuir al éxito operativo diario del almacén, aplicando mis habilidades y experiencia para cumplir con eficacia las tareas asignadas y mantener altos estándares de calidad.
-        </p>
-
-        <p className='text-1xl sm:text-1xl font-bold text-[#ccd6fd]'>¡Creemos algo increíble juntos!</p>
+        <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I'm a full-stack developer.</h2>
+        <p className='text-[#8892b0] py-4 max-w-[700px]'>I'm a passionate entry-level full-stack developer who is embarking on an exciting journey in the field of web development.
+          I enjoy creating clean and efficient code that not only looks great,but functions flawlessly. My goal is to collaborate with other
+          like-minded individuals and businesses to create meaningful solutions that make an impact in the world.</p>
+        <p className='text-1xl sm:text-1xl font-bold text-[#ccd6fd]'>Let's create something great together!</p>
         <div>
           <button className='text-white group border-2 px-6 py-3 my-5 flex items-center hover:bg-[#28bdec] hover:border-[#28bdec]'>View Work
             <span className='duration-300 group-hover:rotate-90'>
